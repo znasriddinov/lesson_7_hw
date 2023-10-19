4853b97288723b9ee1c5cb6bd5ac2f3d54354c18
znasriddinov <143064598+znasriddinov@users.noreply.github.com>
2023-10-19 21:20:55 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// Problem #180
+
+/*
+function MinMax() {
+  console.log(Math.max(1, 2, 10, 5, 6, 7));
+  console.log(Math.min(1, 2, 10, 5, 6, 7));
+}
+
+MinMax();
+*/
+
+// Problem #155
+
+/*
+let Numbers = [2, 6, 9, 7, 5];
+
+console.log(Numbers.shift());
+console.log(Numbers[1]);
+console.log(Numbers.pop());
+*/
